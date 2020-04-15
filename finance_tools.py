6c6adcf0a1fb9c8b9ee46aa4713a9760a87b994c
nielsou <nh.niels@gmail.com>
2020-04-15 18:41:09 +0200

    Add files via upload

---
+++ finance_tools.py
@@ -0,0 +1,8 @@
+# https://pypi.org/project/googlefinance/
+from googlefinance import getQuotes
+# https://aroussi.com/post/python-yahoo-finance
+import yfinance as yf
+import matplotlib.pyplot as plt
+
+
+def remove_weekends
