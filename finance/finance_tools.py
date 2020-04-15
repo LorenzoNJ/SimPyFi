87f2a9ad504bc03ac110f26132e18c729572153d
nielsou <nh.niels@gmail.com>
2020-04-15 18:54:56 +0200

    Deletion of undesired folder

--- finance/finance_tools.py
+++
@@ -1,8 +0,0 @@
-# https://pypi.org/project/googlefinance/
-from googlefinance import getQuotes
-# https://aroussi.com/post/python-yahoo-finance
-import yfinance as yf
-import matplotlib.pyplot as plt
-
-
-def remove_weekends
